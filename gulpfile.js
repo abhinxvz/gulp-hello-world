1215822a56fa017dfd83f40453bcc6d720c82c2f
Abhinav Singh <abhinxv18@gmail.com>
2025-02-26 23:50:27 +0530

    add

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,54 @@
 const gulp = require('gulp');
 const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+
+
 gulp.task('minify-css', () => {
-    return gulp.src('src/*.css') 
+    return gulp.src('src/*.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(gulp.dest('dist')); 
+        .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', gulp.series('minify-css'));
+gulp.task('minify-js', () => {
+    return gulp.src('src/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('optimize-images', () => {
+    return gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'));
+});
+
+
+gulp.task('default', gulp.series('minify-css', 'minify-js', 'optimize-images'));const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+
+
+gulp.task('minify-css', () => {
+    return gulp.src('src/*.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('minify-js', () => {
+    return gulp.src('src/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('optimize-images', () => {
+    return gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'));
+});
+
+
+gulp.task('default', gulp.series('minify-css', 'minify-js', 'optimize-images'));
